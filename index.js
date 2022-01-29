180e7d6a86f2ef3b2e07a571242572ec486b872f
Julesbi <hijules10@gmail.com>
2022-01-29 10:11:08 +0200

    first commited capstone backend

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv');
+const mongoose= require('mongoose');
+//impport routes
+const authRoute=require('./routes/auth');
+const blogRoute= require('./routes/blog');
+dotenv.config();
+
+//connect to DB
+
+mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true},
+()=>console.log('connected to db!')
+);
+//Middlewares
+app.use(express.json());
+
+//routes Middlewares
+app.use('/api/user', authRoute);
+app.use('/api/posts',blogRoute);
+
+app.listen(3000,()=>console.log('server is running...'));
---
+++ routes/auth.js
@@ -0,0 +1,48 @@
+const router=require('express').Router(); 
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const User =require('../model/User');
+const jwt=require('jsonwebtoken');
+const{registerValidation,loginValidation} = require('../validation');
+
+
+router.post('/register',async(req,res)=>{
+   //VALIDATING DATA BEFORE REGISTERING USER
+   const{error}= registerValidation(req.body);
+   if(error) return res.status(400).send(error.details[0].message);
+   // check if the user exist already in database
+   const emailExist= await User.findOne({email:req.body.email});
+   if(emailExist) return res.status(400).send('This email already exists');
+   //hashing password
+   const salt=await bcrypt.genSalt(10);
+   const hashedPassword= await bcrypt.hash(req.body.password,salt);
+   // create new user
+    const user= new User({
+        name:req.body.name,
+        email:req.body.email, 
+        password:hashedPassword
+    });
+    try{
+       const savedUser= await user.save();
+       res.send({user:user._id})
+    }catch(err){
+        res.status(400).send(err);
+    }
+}); 
+//LOGIN
+router.post('/login',async(req,res)=>{
+    //validation first
+    const{error}= loginValidation(req.body);
+   if(error) return res.status(400).send(error.details[0].message);
+   //checking if the email exists
+   const user= await User.findOne({email:req.body.email});
+   if(!user) return res.status(400).send('This email has not signed up');
+   //if password is correct 
+    const validPass= await bcrypt.compare(req.body.password,user.password);
+    if(!validPass)return res.status(400).send('Invalid password')
+    
+    //create and assigning a token
+    const token=jwt.sign({_id:user._id},process.env.TOKEN_SECRET)
+    res.header('auth-token',token).send(token);
+    /* res.send('Logged in') */
+});
+module.exports= router; 
